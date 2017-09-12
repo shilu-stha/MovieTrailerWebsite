1f5c7c66de6db3155fc41a8d1f14225a45a47a52
Shilu Shrestha <sth.shilu@gmail.com>
2017-09-12 10:46:21 +0545

    Fix code blocks

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,8 +3,10 @@ import media
 import requests
 import json
 
+api_key = "e636464b21df7ea6ab6b26b273fea8bc"
 # Make a get request to get the list of latest movies from the Movie Db api.
-response = requests.get("https://api.themoviedb.org/4/list/33436?api_key=e636464b21df7ea6ab6b26b273fea8bc")
+response = requests.get(
+    "https://api.themoviedb.org/4/list/33436?api_key="+api_key)
 
 # Get the json response returned from the api call.
 response_data = response.json()
@@ -13,28 +15,33 @@ response_data = response.json()
 print(response.status_code)
 
 # Start an empty list
-list = []          
+list = []
 
-# Loop through each response results to get all the list of movies and add it in a list
-# according to our Movie model class.
+# Loop through each response results to get all the list of
+# movies and add it in a list according to our Movie model class.
 for db_movie in response_data["results"]:
 
     # get image url from movie db using movies's poster path
     image_url = "https://image.tmdb.org/t/p/w500/"+db_movie["poster_path"]
 
-    # get youtube key from a different api of movie db and then create a final complete youtube url
-    trailer_url = "https://api.themoviedb.org/3/movie/"+str(db_movie["id"])+"/videos?api_key=e636464b21df7ea6ab6b26b273fea8bc&language=en-US"
+    movie_id = str(db_movie["id"])
+    movie_base_url = "https://api.themoviedb.org/3/movie/"
+    base_url_params = "/videos?api_key="+api_key
+
+    # get youtube key from a different api of movie db
+    # and then create a final complete youtube url
+    trailer_url = movie_base_url+movie_id+base_url_params+"&language=en-US"
     trailer_response = requests.get(trailer_url)
     trailer_data = trailer_response.json()["results"]
     youtube_url = "https://www.youtube.com/watch?v="+trailer_data[0]["key"]
 
-    # Create Movie object    
+    # Create Movie object
     movie = media.Movie(db_movie["title"],
                         db_movie["overview"],
                         image_url,
                         youtube_url)
 
     # Use append() to add elements
-    list.append(movie)   
- 
+    list.append(movie)
+
 fresh_tomatoes.open_movies_page(list)
--- media.py
+++ media.py
@@ -1,16 +1,22 @@
 import webbrowser
-class Movie():
 
-    # _ defines that the function is a reserved funtion of python
+
+class Movie():
+    # A model class that holds and encapsulates the data
+    # elements of movies being used in the program
+
+    # _ defines that the function is a reserved funtion
     # self: create a instance, itself
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title,
+                 movie_storyline, poster_image, trailer_youtube):
+        # Constructor of the class Movie which passes required
+        # fields as parameters and sets them in the variable
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        # We are using default webbrowser package which
+        # opens the given url link
         webbrowser.open(self.trailer_youtube_url)
-        
-    
-    
